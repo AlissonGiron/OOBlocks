fade0454621f09441ad9f933e53abbf86e6c9e40
AlissonGiron <alissongiron.hs@gmail.com>
2018-09-27 01:02:12 -0300

    ioorw

--- js/index.js
+++ js/index.js
@@ -33,9 +33,12 @@ function Letter(x, y, pos) {
     this.pos = pos;
 }
 
+function DrawMatrix() {
     $(".led-display").html("");
 
     matrix.forEach(row => {
+        for (j = 0; j < 32; j++) {
+            if (row[j] == "ON") {
                 $(".led-display").append("<span class='led-point led-on'></span>");
             }
             else {
@@ -45,10 +48,13 @@ function Letter(x, y, pos) {
     })
 }
 
+function GenerateMatrix() {
     matrix = [];
 
+    for (i = 0; i < 16; i++) {
         var row = [];
 
+        for (j = 0; j < 32; j++) {
             row.push("OFF");
         }
 
@@ -56,17 +62,22 @@ function Letter(x, y, pos) {
     }
 
     letters.forEach(o => {
+        for (i = 0; i < o.pos.length; i++) {
             var pos = o.pos[i];
 
             var row = (parseInt(i / 3)) + o.x;
             var col = ((i % 3)) + o.y;
 
+            if (pos === "1") {
                 matrix[row][col] = "ON";
             }
         }
 
         o.y--;
+
+        if (o.y == -3) o.y = 40;
     });
+
     DrawMatrix();
 }
 
@@ -86,4 +97,73 @@ function AddRamValue(pos, value)
 }
 
 
+function Initialize() {
+    FillInstructions();
+
+    $('#instructionPicker').sortable({
+        connectWith: '.code-drag',
+        //forcePlaceholderSize: false,
+        revert: true,
+        helper: function (e, li) {
+            copyHelper = li.clone().insertAfter(li);
+            return li.clone();
+        },
+        stop: function () {
+            copyHelper && copyHelper.remove();
+        },
+    });
+
+    $('#code').sortable({
+        revert: true,
+        receive: function (e, ui) {
+            MakeEditable(ui.item);
+            copyHelper = null;
+        }
+    });
+}
+
+function FillInstructions() {
+    let divPicker = $('#instructionPicker');
+
+    for (let instruction in InstructionCodes) {
+        let template = $($('#instructionSrc').html());
+
+        template.attr('instruction-code', InstructionCodes[instruction]);
+        template.find('.instruction-desc').text(instruction);
+
+        divPicker.append(template);
+    }
+}
+
+function MakeEditable(block) {
+    let btnDelete = $($('#btnDelete').html());
+    btnDelete.on('click', function(e) {
+        $(e.target).closest('.instruction-block').remove();
+    })
+
+    block.prepend(btnDelete);
+
+    if (new Instruction(block.attr('instruction-code')).hasOperand) {
+        block.append($('#inputInstructionValue').html());
+    }
+}
+
+function Execute() {
+    let instructions = [];
+    let interpreter = new AssemblyInterpreter();
+
+    interpreter.state.RAM[0] = 356;
+    interpreter.state.RAM[1] = 1;
+
+    $('#code > .instruction-block').each((i, v) => {
+        let code = $(v).attr('instruction-code');
+        let operator = $(v).find('input').val();
+
+        instructions.push(new Instruction(code, operator));
+    });
+
+    interpreter.interpret(instructions, (curState) => {
+        // chamado na execução de cada instrução
+    });
+}
 
